a642f88800c1b4bd9cac62b5d9a7e3ccec0ca7a2
MatiasSlotboom <matias.slotboom@helsinki.fi>
2025-03-20 17:49:40 +0200

    Viikko2: Tehtävä 7

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,27 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.00)
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 15.00)
+
+    def test_kortilta_rahan_ottaminen_vahentaa_saldoa_oikein(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 5.00)
+
+    def test_kortilta_rahan_ottaminen_ei_vahenna_saldoa_jos_ei_riittavasti(self):
+        self.maksukortti.ota_rahaa(2000)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_kortilta_rahan_ottaminen_palauttaa_true_jos_riittaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+
+    def test_kortilta_rahan_ottaminen_palauttaa_false_jos_riittaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(100000), False)
+
+    def test_str_metodi_palauttaa_oikean_merkkijonon(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
